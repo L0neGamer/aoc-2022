ec4e9199af95d37f5b33581d43735c19fbb6462c
L0neGamer <benjaminbwm@gmail.com>
2022-12-06 18:20:46 +0000

    day1 part 1

---
+++ src/day01.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::io::Read;
+use std::io::Error;
+use std::num::ParseIntError;
+use typestuff;
+
+pub fn day01 (filename: String) -> Option<i32> {
+    println!("{:?}", std::env::current_dir());
+    let mut file = File::open(filename).ok()?;
+    let mut str: String = "".to_string();
+    file.read_to_string(&mut str);
+    let split: Vec<_> = str.split("\n").map(|x| x.to_string()).collect();
+    let packs = to_packs(split).ok()?;
+
+    println!("packs {:?}", packs);
+    let max_elf = find_max(packs)?;
+    println!("packs {:?}", max_elf);
+
+    let m = max_elf.cals.iter().sum();
+
+    Some(m)
+}
+
+#[derive(Debug, Clone)]
+struct ElfPack {
+    cals: Vec<i32>
+}
+
+fn to_packs(strs: Vec<String>) -> Result<Vec<ElfPack>, String> {
+    let collected: Vec<&[String]> = strs.split(|x| x == "").collect();
+
+    let packs = collected.iter().map(|x| to_elfpack(x.to_vec())).into_iter().collect();
+    match packs {
+        Ok(elfpacks) => Ok(elfpacks),
+        Err(_) => Err("parseerr".to_string())
+    }
+}
+
+fn to_elfpack(strs: Vec<String>) -> Result<ElfPack, ParseIntError> {
+    let errs: Vec<i32> = strs.iter().map(|s| s.parse()).into_iter().collect::<Result<Vec<i32>,_>>()?;
+    Ok(ElfPack{cals: errs})
+}
+
+fn find_max(packs: Vec<ElfPack>) -> Option<ElfPack> {
+    let with_max = packs.iter().map(|pack| (pack, pack.cals.iter().sum::<i32>()));
+    let found_max = with_max.reduce(|(ep, i), (ep2,i2)| if i2 > i {(ep2,i2)} else {(ep,i)})?.0;
+    Some((*found_max).clone())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod day01;
+
 fn main() {
     println!("Hello, world!");
+    println!("{:?}",day01::day01("./inputs/day01_example".to_string()));
+    println!("{:?}",day01::day01("./inputs/day01_1".to_string()));
 }
