e5cccdfc7811a0eabbaa32146e47259786de7eb1
L0neGamer <benjaminbwm@gmail.com>
2022-12-06 19:26:13 +0000

    better basics

--- src/day01.rs
+++ src/day01.rs
@@ -1,14 +1,13 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, BufReader, BufRead};
 use std::num::ParseIntError;
 use std::iter::Iterator;
 
 pub fn day01 (filename: String) -> Option<i32> {
     println!("{:?}", std::env::current_dir());
-    let mut file = File::open(filename).ok()?;
-    let mut str: String = "".to_string();
-    file.read_to_string(&mut str).ok()?;
-    let split: Vec<_> = str.split('\n').map(|x| x.to_string()).collect();
+    let file = File::open(filename).ok()?;
+    let file2 = BufReader::new(file);
+    let split: Vec<_> = file2.lines().into_iter().collect::<Result<Vec<String>,_>>().ok()?;
     let packs = to_packs(split).ok()?;
 
     println!("packs {packs:?}");
